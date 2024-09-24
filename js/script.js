7a15230b516da050d0cd40a2c6fe43cddb64f196
Vladislav <64088661+alteropt@users.noreply.github.com>
2024-09-24 14:44:47 +0300

    4th section + linking

--- js/script.js
+++ js/script.js
@@ -1,28 +1,109 @@
-const swiper = new Swiper(document.querySelector('.swiper'), {
+const swiperConstructor = new Swiper(document.querySelector('.constructor .constructor__variants'), {
   direction: 'horizontal',
   slidesToShow: 1,
   spaceBetween: 12,
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.constructor__choice .swiper-button-next',
+    prevEl: '.constructor__choice .swiper-button-prev',
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.constructor choice .swiper-pagination',
     clickable: true,
   }
 })
 
-const currentVariantText = document.querySelector('.constructor__current span')
-const swiperBullets = document.querySelectorAll('.swiper-pagination-bullet') 
-const swiperLength = swiper.slides.length
+const swiperConstructor2 = new Swiper(document.querySelector('.constructor.shevron .constructor__variants'), {
+  direction: 'horizontal',
+  slidesToShow: 1,
+  spaceBetween: 12,
+  loop: true,
+  navigation: {
+    nextEl: '.constructor.shevron-left .constructor__choice .swiper-button-next',
+    prevEl: '.constructor.shevron-left .constructor__choice .swiper-button-prev',
+  },
+  pagination: {
+    el: '.constructor.shevron-left .swiper-pagination',
+    clickable: true,
+  }
+})
 
-swiper.on('slideChange afterInit init', () => {
-  const activeSlideIndex = Array.from(swiperBullets).indexOf(document.querySelector('.swiper-pagination-bullet-active'))
-  
+const swiperConstructor3 = new Swiper(document.querySelector('.constructor.shevron-right .constructor__variants'), {
+  direction: 'horizontal',
+  slidesToShow: 1,
+  spaceBetween: 12,
+  loop: true,
+  navigation: {
+    nextEl: '.constructor.shevron-right .constructor__choice .swiper-button-next',
+    prevEl: '.constructor.shevron-right .constructor__choice .swiper-button-prev',
+  },
+  pagination: {
+    el: '.constructor.shevron-right .constructor__choice .swiper-pagination',
+    clickable: true,
+  }
+})
+
+const currentVariantText = document.querySelector('.constructor.shevron-left .constructor__current span')
+const swiperLength = swiperConstructor2.slides.length
+
+swiperConstructor2.on('slideChange afterInit init', () => {
+  let swiperBullets = document.querySelectorAll('.constructor.shevron-left .swiper-pagination-bullet') 
+  const activeSlideIndex = Array.from(swiperBullets).indexOf(document.querySelector('.constructor.shevron-left .swiper-pagination-bullet-active'))
+
   if(activeSlideIndex + 1 === swiperLength) {
   currentVariantText.innerHTML = ` пуст`
   } else {
     currentVariantText.innerHTML = `${activeSlideIndex+1}/${swiperLength-1}`
   }
+})
+
+const cardSwiper = new Swiper(document.querySelector('.card__choice'), {
+  direction: 'horizontal',
+  slidesToShow: 1,
+  spaceBetween: 12,
+  loop: true,
+
+  navigation: {
+    nextEl: '.card__choice .swiper-button-next',
+    prevEl: '.card__choice .swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.card__choice .swiper-pagination',
+    clickable: true,
+  }
+})
+
+document.getElementById('patch-name').addEventListener('change', function(e) {
+  let patchName = e.target.value.trim()
+  document.querySelector('.card__name').textContent = patchName
+  e.target.value = e.target.value.trim()
+  
+  let charsLeft = 26 - e.target.value.length
+  document.getElementById('name-amount').textContent = charsLeft
+
+  if(document.querySelector('.card__name').textContent.length > 13) {
+    document.querySelector('.card__name').innerHTML = document.querySelector('.card__name').textContent.slice(0, 13) + `<br/>` + document.querySelector('.card__name').textContent.slice(13,)
+  }
+})
+
+document.getElementById('patch-name').addEventListener('keydown', function(e) {
+  let patchName = e.target.value.trim()
+  e.target.value = e.target.value.trim()
+
+  if(e.code === "Enter") {    
+    e.preventDefault()
+    document.querySelector('.card__name').textContent = patchName
+    
+    let charsLeft = 26 - e.target.value.length
+    document.getElementById('name-amount').textContent = charsLeft
+    
+    if(document.querySelector('.card__name').textContent.length > 13) {
+      document.querySelector('.card__name').innerHTML = document.querySelector('.card__name').textContent.slice(0, 13) + `<br/>` + document.querySelector('.card__name').textContent.slice(13,)
+    }
+  }
+})
+
+const mainSwiper = new Swiper('.main-slider', {
+  autoHeight: true
 })
